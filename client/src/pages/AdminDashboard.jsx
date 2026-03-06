9f482f5d4acf5266eaa8f04c0187eae16ec75bcd
Naphat34 <naphatsrirak@hotmail.com>
2026-03-07 00:27:49 +0700

    ViewScore

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -95,7 +95,7 @@ export default function AdminDashboard() {
                         <MenuButton active={activeTab === 'matches'} onClick={() => setActiveTab('matches')} icon={<Swords size={20} />} label="Matches" darkMode={darkMode} />
                         <MenuButton active={activeTab === 'escore'} onClick={() => setActiveTab('escore')} icon={<Star size={20} />} label="VIS" darkMode={darkMode} />
                         <MenuButton active={activeTab === 'live_scorer'} onClick={() => setActiveTab('live_scorer')} icon={<PlayCircle size={20} />} label="Live Scorer" darkMode={darkMode} />
-                        <MenuButton active={activeTab === 'control_panel'} onClick={() => setActiveTab('control_panel')} icon={<Settings size={20} />} label="Control Panel" darkMode={darkMode} />
+                        <MenuButton active={activeTab === 'ScoreViewReferee'} onClick={() => setActiveTab('ScoreViewReferee')} icon={<Settings size={20} />} label="Score Referee" darkMode={darkMode} />
                     </div>
 
                     <div className={`border-b mx-4 mb-4 ${darkMode ? 'border-gray-700' : 'border-gray-200'}`}></div>
